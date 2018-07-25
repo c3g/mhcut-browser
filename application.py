b88005ea35b2e519f170205eb0c6bc2241ccfa17
David Lougheed <david.lougheed@gmail.com>
2018-07-25 14:19:40 -0400

    Remove unused ceil import in application.py

--- application.py
+++ application.py
@@ -3,7 +3,6 @@
 import re
 import sqlite3
 from flask import Flask, g, json, request
-from math import ceil
 from typing import Pattern
 
 CHR_DOMAIN = re.compile("^(chr(1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|X|Y)|any)$")

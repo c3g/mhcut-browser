41cd134c1743bc74b97e59c19f9503daa5440fc5
David Lougheed <david.lougheed@gmail.com>
2018-07-27 17:11:16 -0400

    Add WIP setup bash script.

---
+++ setup.bash
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -eu
+
+if ! [ -x "$(command -v python3)" ]; then
+  echo "Attempting to install Python 3 via apt..."
+  sudo apt-get install -y python3 python3-dev
+fi
+
+if ! [ -x "$(command -v pip3)" ]; then
+    echo "Attempting to install pip3 via apt..."
+    sudo apt-get install -y python3-pip
+fi
+
+if ! [ -x "$(command -v virtualenv)" ]; then
+    echo "Attempting to install virtualenv via pip..."
+    sudo -H pip3 install virtualenv
+fi
+
+# NGINX?
+
+if ! [ -x "$(command -v npm)" ]; then
+  echo "Attempting to install NPM from nodesource via apt..."
+  curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+  sudo apt-get install -y nodejs
+fi
+
+echo "Updating NPM to the latest version..."
+sudo npm install -g npm
+
+echo "Setting up Python 3 virtual environment in project root..."
+virtualenv -p python3 ./env
+source env/bin/activate
+pip install -r requirements.txt
+
+echo "Installing web dependencies via NPM..."
+cd web
+npm install
+echo "Building JavaScript bundle..."
+npm run build-dev
+cd ..
+
+echo "Setup complete."
+echo
+
+echo "Please activate the virtual environment by running the following:"
+echo "    source env/bin/activate"
+echo

4db3447381c8004b30c3a8c4f6ec38291f67475f
David Lougheed <david.lougheed@gmail.com>
2018-08-08 17:22:50 -0400

    Add performance improvements to tsv_to_sqlite script and add indices to variants

---
+++ indices.sql
@@ -0,0 +1,3 @@
+CREATE INDEX variants_chr_idx ON variants(chr);
+CREATE INDEX variants_start_idx ON variants(start);
+CREATE INDEX variants_end_idx ON variants(end);
--- tsv_to_sqlite.py
+++ tsv_to_sqlite.py
@@ -7,6 +7,14 @@ import sys
 from tqdm import tqdm
 
 
+INSERT_VARIANTS_QUERY = ("INSERT INTO variants(chr, start, end, rs, caf, topmed, gene_info, pm, mc, af_exac, af_tgp, "
+                         "                     allele_id, clndn, clnsig, dbvarid, gene_info_clinvar, mc_clinvar, "
+                         "                     citation, geneloc, var_l, mh_l, mh_1l, hom, nbmm, mh_dist, mh_seq_1, "
+                         "                     mh_seq_2, pam_mot, pam_uniq, guides_no_ot, guides_min_ot) "
+                         "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,"
+                         "       ?, ?, ?)")
+
+
 def int_or_none_cast(x):
     """
     Casts a provided value to an integer, and returns None if the cast fails.
@@ -34,6 +42,11 @@ def main():
     with open("./schema.sql", "r") as s:
         c.executescript(s.read())
 
+    # Disable blocking write-to-disk at the cost of a chance of database corruption (which is fine, since we have the
+    # original files) in order to improve speed.
+    c.execute("PRAGMA synchronous=OFF")
+    c.execute("PRAGMA journal_mode=MEMORY")
+
     # Get number of lines for progress bars.
     n_variants = 0
     n_guides = 0
@@ -48,7 +61,9 @@ def main():
         for _ in gs_file:
             n_guides += 1
 
-    with open(sys.argv[1], "r") as vs_file:
+    item_bank = []
+
+    with open(sys.argv[1], "r", newline="") as vs_file:
         reader = csv.DictReader(vs_file, delimiter="\t")
         i = 0
         for variant in tqdm(reader, total=n_variants, desc="variants"):  # TODO: Find total with wc -l
@@ -56,28 +71,35 @@ def main():
             if gene_info_clinvar == "NA":
                 # Treat NA, but not -, as null
                 gene_info_clinvar = None
-            c.execute("INSERT INTO variants(chr, start, end, rs, caf, topmed, gene_info, pm, mc, af_exac, af_tgp, "
-                      "                     allele_id, clndn, clnsig, dbvarid, gene_info_clinvar, mc_clinvar, "
-                      "                     citation, geneloc, var_l, mh_l, mh_1l, hom, nbmm, mh_dist, mh_seq_1, "
-                      "                     mh_seq_2, pam_mot, pam_uniq, guides_no_ot, guides_min_ot) "
-                      "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, "
-                      "       ?, ?)",
-                      (variant["chr"].strip(), int(variant["start"]), int(variant["end"]),
-                       int_or_none_cast(variant["RS"]), variant["CAF"].strip(), variant["TOPMED"].strip(),
-                       variant["GENEINFO"].strip(), variant["PM"].strip(), variant["MC"].strip(),
-                       variant["AF_EXAC"].strip(), variant["AF_TGP"].strip(), variant["ALLELEID"].strip(),
-                       variant["CLNDN"].strip(), variant["CLNSIG"].strip(), variant["DBVARID"].strip(),
-                       gene_info_clinvar, variant["MC.ClinVar"].strip(), variant["citation"].strip(),
-                       variant["geneloc"].strip(), int(variant["varL"]), int(variant["mhL"]), int(variant["mh1L"]),
-                       variant["hom"].strip(), int(variant["nbMM"]), int_or_none_cast(variant["mhDist"]),
-                       variant["MHseq1"].strip(), variant["MHseq2"].strip(), int_or_none_cast(variant["pamMot"]),
-                       int_or_none_cast(variant["pamUniq"]), int_or_none_cast(variant["guidesNoOT"]),
-                       int_or_none_cast(variant["guidesMinOT"])))
+            item_bank.append((variant["chr"], int(variant["start"]), int(variant["end"]),
+                              int_or_none_cast(variant["RS"]), variant["CAF"], variant["TOPMED"], variant["GENEINFO"],
+                              variant["PM"], variant["MC"], variant["AF_EXAC"], variant["AF_TGP"], variant["ALLELEID"],
+                              variant["CLNDN"], variant["CLNSIG"], variant["DBVARID"], gene_info_clinvar,
+                              variant["MC.ClinVar"], variant["citation"], variant["geneloc"], int(variant["varL"]),
+                              int(variant["mhL"]), int(variant["mh1L"]), variant["hom"], int(variant["nbMM"]),
+                              int_or_none_cast(variant["mhDist"]), variant["MHseq1"], variant["MHseq2"],
+                              int_or_none_cast(variant["pamMot"]), int_or_none_cast(variant["pamUniq"]),
+                              int_or_none_cast(variant["guidesNoOT"]), int_or_none_cast(variant["guidesMinOT"])))
+
+            if i % 100 == 0:
+                c.executemany(INSERT_VARIANTS_QUERY, item_bank)
+                item_bank = []
+
             i += 1
+
+            # Commit every once in a while when loading large files.
+            if i % 1000000 == 0:
+                conn.commit()
+
+    if len(item_bank) > 0:
+        c.executemany(INSERT_VARIANTS_QUERY, item_bank)
+
+    with open("./indices.sql", "r") as indices_file:
+        c.executescript(indices_file.read())
 
     conn.commit()
 
-    with open(sys.argv[2], "r") as gs_file:
+    with open(sys.argv[2], "r", newline="") as gs_file:
         reader = csv.DictReader(gs_file, delimiter="\t")
         for guide in tqdm(reader, total=n_guides, desc="guides"):
             c.execute("SELECT id FROM variants WHERE chr = :chr AND start = :start AND end = :end", {
@@ -89,11 +111,12 @@ def main():
             c.execute("INSERT INTO guides(variant_id, protospacer, mm0, mm1, mm2, m1_dist_1, m1_dist_2, mh_dist_1, "
                       "mh_dist_2, nb_off_tgt, largest_off_tgt, bot_score, bot_size, bot_var_l, bot_gc, bot_seq)"
                       "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                      (variant_id, guide["protospacer"].strip(), int_or_none_cast(guide["mm0"]),
+                      (variant_id, guide["protospacer"], int_or_none_cast(guide["mm0"]),
                        int_or_none_cast(guide["mm1"]), int_or_none_cast(guide["mm2"]), int(guide["m1Dist1"]),
-                       int(guide["m1Dist2"]), int(guide["mhDist1"]), int(guide["mhDist2"]), int(guide["nbOffTgt"]),
-                       int(guide["largestOffTgt"]), guide["botScore"].strip(), int_or_none_cast(guide["botSize"]),
-                       int_or_none_cast(guide["botVarL"]), int_or_none_cast(guide["botGC"]), guide["botSeq"].strip()))
+                       int(guide["m1Dist2"]), int(guide["mhDist1"]), int(guide["mhDist2"]),
+                       int_or_none_cast(guide["nbOffTgt"]), int_or_none_cast(guide["largestOffTgt"]), guide["botScore"],
+                       int_or_none_cast(guide["botSize"]), int_or_none_cast(guide["botVarL"]),
+                       int_or_none_cast(guide["botGC"]), guide["botSeq"]))
 
     conn.commit()
     conn.close()

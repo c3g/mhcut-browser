42faf3cb517ddcfdbca34cbe51f8eef01851b49d
David Lougheed <david.lougheed@gmail.com>
2018-08-16 16:50:37 -0400

    Create custom enum types and add some indices to schema. geneloc -> location
    Fix topmed index

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,16 +1,18 @@
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
 DROP TABLE IF EXISTS variants CASCADE;
 DROP TABLE IF EXISTS guides CASCADE;
 DROP TABLE IF EXISTS summary_statistics CASCADE;
 
-DROP INDEX IF EXISTS variants_chr_idx;
 DROP INDEX IF EXISTS variants_start_idx;
 DROP INDEX IF EXISTS variants_end_idx;
-DROP INDEX IF EXISTS variants_chr_start_end_idx;
+DROP INDEX IF EXISTS variants_chr_start_end_rs_idx;
 DROP INDEX IF EXISTS variants_mh_l_start_end_idx;
 DROP INDEX IF EXISTS variants_rs_idx;
 DROP INDEX IF EXISTS variants_caf_idx;
 DROP INDEX IF EXISTS variants_topmed_idx;
 DROP INDEX IF EXISTS variants_gene_info_idx;
+DROP INDEX IF EXISTS variants_gene_info_trgm_idx;
 DROP INDEX IF EXISTS variants_pm_idx;
 DROP INDEX IF EXISTS variants_mc_idx;
 DROP INDEX IF EXISTS variants_af_exac_idx;
@@ -20,30 +22,46 @@ DROP INDEX IF EXISTS variants_clndn_idx;
 DROP INDEX IF EXISTS variants_clnsig_idx;
 DROP INDEX IF EXISTS variants_dbvarid_idx;
 DROP INDEX IF EXISTS variants_gene_info_clinvar_idx;
+DROP INDEX IF EXISTS variants_gene_info_clinvar_trgm_idx;
 DROP INDEX IF EXISTS variants_mc_clinvar_idx;
 DROP INDEX IF EXISTS variants_citation_idx;
 DROP INDEX IF EXISTS variants_geneloc_idx;
+DROP INDEX IF EXISTS variants_var_l_idx;
 DROP INDEX IF EXISTS variants_mh_l_idx;
+DROP INDEX IF EXISTS variants_mh_1l_idx;
+DROP INDEX IF EXISTS variants_hom_idx;
+DROP INDEX IF EXISTS variants_nbmm_idx;
+DROP INDEX IF EXISTS variants_mh_dist_idx;
+DROP INDEX IF EXISTS variants_mh_seq_1_idx;
+DROP INDEX IF EXISTS variants_mh_seq_2_idx;
+DROP INDEX IF EXISTS variants_pam_mot_idx;
 DROP INDEX IF EXISTS variants_pam_uniq_idx;
 DROP INDEX IF EXISTS variants_guides_no_ot_idx;
 DROP INDEX IF EXISTS variants_guides_min_ot_idx;
 
 DROP INDEX IF EXISTS guides_variant_id_idx;
 
+DROP TYPE IF EXISTS CHROMOSOME;
+DROP TYPE IF EXISTS GENE_LOCATION;
+
+CREATE TYPE CHROMOSOME AS ENUM ('chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10',
+                                'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19',
+                                'chr20', 'chr21', 'chr22', 'chrX', 'chrY');
+
+CREATE TYPE VARIANT_LOCATION AS ENUM ('intergenic', 'intronic', 'exonic');
+
 CREATE TABLE variants (
   id INTEGER PRIMARY KEY,
-  chr TEXT NOT NULL CHECK (chr IN ('chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10',
-                                   'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19',
-                                   'chr20', 'chr21', 'chr22', 'chrX', 'chrY')),
+  chr CHROMOSOME NOT NULL,
   pos_start INTEGER NOT NULL CHECK (pos_start >= 0),
   pos_end INTEGER NOT NULL CHECK (pos_end >= 0),
-  rs TEXT,
+  rs TEXT, -- NULL means "-"
   caf TEXT, -- TODO: WHAT IS THIS?
   topmed TEXT, -- TODO: WHAT IS THIS?
   gene_info TEXT, -- TODO: WHAT IS THIS?
   pm TEXT, -- TODO: WHAT IS THIS? - NA VS. "-"
   mc TEXT, -- TODO: NA VS. "-"?
-  af_exac TEXT,
+  af_exac TEXT, -- NULL means NA
   af_tgp TEXT,
   allele_id INTEGER CHECK (allele_id >= 0),
   clndn TEXT,
@@ -52,7 +70,7 @@ CREATE TABLE variants (
   gene_info_clinvar TEXT, -- NA is represented as NULL, '-' is left as-is
   mc_clinvar TEXT,
   citation TEXT,
-  geneloc TEXT NOT NULL CHECK (geneloc IN ('intronic', 'exonic', 'intergenic')),
+  location VARIANT_LOCATION NOT NULL,
   var_l INTEGER NOT NULL CHECK (var_l >= 0), -- Variant Size
   mh_l INTEGER NOT NULL CHECK (mh_l >= 0), -- Micro-Homology Length
   mh_1l INTEGER NOT NULL CHECK (mh_1l >= 0), -- Number of First Consecutive Matches
@@ -69,7 +87,7 @@ CREATE TABLE variants (
 
 CREATE TABLE guides (
   id INTEGER PRIMARY KEY,
-  variant_id INTEGER NOT NULL REFERENCES variants,
+  variant_id INTEGER NOT NULL REFERENCES variants ON DELETE CASCADE,
   protospacer TEXT,
   mm0 INTEGER, -- NULL means NA
   mm1 INTEGER, -- NULL means NA
--- sql/variants_indices.sql
+++ sql/variants_indices.sql
@@ -1,12 +1,13 @@
-CREATE INDEX variants_chr_idx ON variants(chr);
+-- noinspection SqlResolveForFile
+
 CREATE INDEX variants_start_idx ON variants(pos_start);
 CREATE INDEX variants_end_idx ON variants(pos_end);
-CREATE INDEX variants_chr_start_end_idx ON variants(chr, pos_start, pos_end);
-CREATE INDEX variants_mh_l_start_end_idx ON variants(mh_l, pos_start, pos_end);
+CREATE UNIQUE INDEX variants_chr_start_end_rs_idx ON variants(chr, pos_start, pos_end, rs);
 CREATE INDEX variants_rs_idx ON variants(rs);
 CREATE INDEX variants_caf_idx ON variants(caf);
-CREATE INDEX variants_topmed_idx ON variants(caf);
+CREATE INDEX variants_topmed_idx ON variants(topmed);
 CREATE INDEX variants_gene_info_idx ON variants(gene_info);
+CREATE INDEX variants_gene_info_trgm_idx ON variants USING gin(gene_info gin_trgm_ops) WHERE gene_info IS NOT NULL;
 CREATE INDEX variants_pm_idx ON variants(pm);
 CREATE INDEX variants_mc_idx ON variants(mc);
 CREATE INDEX variants_af_exac_idx ON variants(af_exac);
@@ -16,10 +17,20 @@ CREATE INDEX variants_clndn_idx ON variants(clndn);
 CREATE INDEX variants_clnsig_idx ON variants(clnsig);
 CREATE INDEX variants_dbvarid_idx ON variants(dbvarid);
 CREATE INDEX variants_gene_info_clinvar_idx ON variants(gene_info_clinvar);
+CREATE INDEX variants_gene_info_clinvar_trgm_idx ON variants
+  USING gin(gene_info_clinvar gin_trgm_ops) WHERE gene_info_clinvar IS NOT NULL;
 CREATE INDEX variants_mc_clinvar_idx ON variants(mc_clinvar);
 CREATE INDEX variants_citation_idx ON variants(citation);
-CREATE INDEX variants_geneloc_idx ON variants(geneloc);
-CREATE INDEX variants_mh_l_idx ON variants(mh_l);
+CREATE INDEX variants_location_idx ON variants(location);
+CREATE INDEX variants_var_l_idx ON variants(var_l);
+CREATE INDEX variants_mh_l_start_end_idx ON variants(mh_l, pos_start, pos_end);
+CREATE INDEX variants_mh_1l_idx ON variants(mh_1l);
+CREATE INDEX variants_hom_idx ON variants(hom);
+CREATE INDEX variants_nbmm_idx ON variants(nbmm);
+CREATE INDEX variants_mh_dist_idx ON variants(mh_dist);
+CREATE INDEX variants_mh_seq_1_idx ON variants(mh_seq_1);
+CREATE INDEX variants_mh_seq_2_idx ON variants(mh_seq_2);
+CREATE INDEX variants_pam_mot_idx ON variants(pam_mot);
 CREATE INDEX variants_pam_uniq_idx ON variants(pam_uniq);
 CREATE INDEX variants_guides_no_ot_idx ON variants(guides_no_ot) WHERE guides_no_ot IS NOT NULL;
 CREATE INDEX variants_guides_min_ot_idx ON variants(guides_min_ot) WHERE guides_min_ot IS NOT NULL;

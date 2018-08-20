fa8ecd72b4556a68622ec3e49ab7dd0620d270e7
David Lougheed <david.lougheed@gmail.com>
2018-08-20 10:40:50 -0400

    Add environment variable setup (untested) to wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
+import os
+
 from application import app as application
+
+os.environ["DB_USER"] = "your_production_db_user"
+os.environ["DB_PASSWORD"] = "your_production_db_password"
 
 if __name__ == "__main__":
     application.run()

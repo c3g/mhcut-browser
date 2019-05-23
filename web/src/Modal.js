5b1c3b674a35da850028fa17710daa3ab4d15f41
David Lougheed <david.lougheed@gmail.com>
2019-05-23 12:24:36 -0400

    Add event system for modals

--- web/src/Modal.js
+++ web/src/Modal.js
@@ -3,6 +3,7 @@ import * as d3 from "d3";
 export default class {
     constructor(selector) {
         this.container = d3.select(selector);
+        this.listeners = {};
 
         this.container.on("click", () => this.hide());
         this.container.select(".modal").on("click", () => d3.event.stopPropagation());
@@ -15,11 +16,23 @@ export default class {
         });
     }
 
+    addListener(event, func) {
+        if (!Object.keys(this.listeners).includes(event)) this.listeners[event] = [];
+        this.listeners[event].push(func);
+    }
+
+    dispatch(event) {
+        if (!Object.keys(this.listeners).includes(event)) return;
+        this.listeners[event].forEach(f => f());
+    }
+
     show() {
         this.container.classed("shown", true);
+        this.dispatch("modalShow");
     }
 
     hide() {
         this.container.classed("shown", false);
+        this.dispatch("modalHide");
     }
 }

a438cc7777c76c3ac665ed068065f1896d37b694
David Lougheed <david.lougheed@gmail.com>
2019-06-07 10:39:39 -0400

    fix wsgi template

--- wsgi.py
+++ wsgi.py
@@ -8,7 +8,8 @@ os.environ["DB_NAME"] = "your_production_db_name"
 os.environ["DB_USER"] = "your_production_db_user"
 os.environ["DB_PASSWORD"] = "your_production_db_password"
 
-os.environ["SENDGRID_API_KEY"] = "your_production_sendgrid_api_key"
+os.environ["GMAIL_SENDER_EMAIL"] = "your_production_gmail_sender_email"
+os.environ["GMAIL_SENDER_PASSWORD"] = "your_production_gmail_sender_password"
 os.environ["BUG_REPORT_EMAIL"] = "your_production_bug_report_email"
 
 if __name__ == "__main__":
